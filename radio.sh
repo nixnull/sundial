f07051ba146759146dd82958823fe0eea82e4f0a
nixnull <nixnull@protonmail.com>
2020-08-01 18:49:27 -0500

    version so far

---
+++ radio.sh
@@ -0,0 +1,5 @@
+python3 weather.py
+cat weather.txt
+espeak -f weather.txt
+mplayer https://www.stlpublicradio.org/newscast.mp3
+mplayer http://kdhx-ice.streamguys1.com/live
---
+++ weather.py
@@ -0,0 +1,73 @@
+# Python program to find current 
+# weather details of any city 
+# using openweathermap api 
+
+# import required modules 
+import requests, json 
+from datetime import datetime
+
+# Enter your API key here 
+api_key = "319bfead6999413d89c27d21b6ab2577"
+
+# base_url variable to store url 
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+# Give city name 
+city_name = "St. Louis"
+
+# complete_url variable to store 
+# complete url address 
+complete_url = base_url + "appid=" + api_key + "&q=" + city_name 
+
+# get method of requests module 
+# return response object 
+response = requests.get(complete_url)
+
+# json method of response object 
+# convert json format data into 
+# python format data 
+x = response.json() 
+
+# Now x contains list of nested dictionaries 
+# Check the value of "cod" key is equal to 
+# "404", means city is found otherwise, 
+# city is not found 
+if x["cod"] != "404": 
+    print(str(x))
+    # store the value of "main" 
+    # key in variable y 
+    y = x["main"] 
+
+    # store the value of "weather" 
+    # key in variable z 
+    z = x["weather"] 
+    
+    # time data was recorded (NOT retrieved)
+    the_time = datetime.fromtimestamp(x["dt"]).strftime('%H:%M')
+
+    # time of sunset
+    sunset = datetime.fromtimestamp(x["sys"]["sunset"]).strftime('%H:%M')
+    
+    # store the value corresponding 
+    # to the "description" key at 
+    # the 0th index of z 
+    weather_description = z[0]["description"] 
+
+    # print following values 
+    to_write = ("Time: " + the_time + ".\n" +
+                    "Current Temperature: " +
+                    str(y["temp"]) +
+                    " kelvin." +
+                    "\nFeels Like: " +
+                    str(y["feels_like"]) +
+                    " kelvin." +
+                    "\n" +
+                    "Currently " + str(weather_description) + "." +
+                    "\n" +
+                    "Sunset: " + sunset + "\n")
+                    
+    with open("weather.txt", 'w') as f:
+        f.write(to_write)
+
+else: 
+    print(" City Not Found ") 
